3f6652795dca3d20e0ccc1f274298b7ca90042fb
Abel <90abel.avr@gmail.com>
2022-07-10 02:33:58 -0300

    add listado pacientes

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,24 @@
+const Paciente = () => {
+  return (
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {' '}
+            <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {' '}
+            <span className="font-normal normal-case">Abel</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {' '}
+            <span className="font-normal normal-case">correo@correo.com</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha de alta: {' '}
+            <span className="font-normal normal-case">10 de julio de 2022</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {' '}
+            <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae alias necessitatibus sit nulla similique est obcaecati itaque veniam error accusantium, dolore placeat tenetur qui a? Quaerat accusantium inventore dolor magni.</span>
+        </p>
+        </div>
+
+  )
+}
+
+export default Paciente
